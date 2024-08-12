ad86fae753f604c05e0ade70f4f4dff1e46c1104
Nathan Rodger <Nathan.Rodger@connellsgroup.co.uk>
2024-08-12 10:52:34 +0100

    Added images to gallery

--- src/script/gallery/gallery.js
+++ src/script/gallery/gallery.js
@@ -1,5 +1,8 @@
+import Images from '../../asset/images/propertywide/*.png';
 import PhotoSwipeLightbox from 'photoswipe/dist/photoswipe-lightbox.esm.js';
 import 'photoswipe/dist/photoswipe.css';
+
+const gallery = document.querySelector('#gallery');
 
 const lightbox = new PhotoSwipeLightbox({
     gallery: '#gallery',
@@ -7,4 +10,18 @@ const lightbox = new PhotoSwipeLightbox({
     pswpModule: () => import('photoswipe')
 });
 
+const createGalleryItem = (url, text) => {
+    return (
+        `<a href="${ url }" class="anchor card__link" target="_blank"
+            data-pswp-src="${ url }" data-pswp-width="360" data-pswp-height="638">
+            ${ text }
+        </a>`
+    );
+};
+
 lightbox.init();
+
+Object.entries(Images).forEach(([key, value], index) => {
+    const text = index === 0 ? 'View Gallery' : '';
+    gallery.innerHTML += createGalleryItem(value, text);
+});
